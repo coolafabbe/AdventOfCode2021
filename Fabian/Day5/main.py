6ee6ba3285b389cfe6cc46e1359185d237d4881e
Fabian Fasth <fabian.fasth@gmail.com>
2021-12-05 18:04:33 +0100

    day 5 complete

--- Fabian/Day5/main.py
+++ Fabian/Day5/main.py
@@ -4,45 +4,41 @@ import os
 import re
 
 path = os.path.dirname(os.path.realpath(__file__))
-#os.system('cls')
-
-STRAIGHLINES = True
 
 dim_max = 0
 with open(path+"/input.txt") as file:
   input = file.readlines()
   
-  lines = []
-  for line in input:
-    line = re.split("-> |,", line)
+lines = []
+for line in input:
+  line = re.split("-> |,", line)
 
-    x = [int(line[0]), int(line[2])]
-    y = [int(line[1]), int(line[3])]
+  x = [int(line[0]), int(line[2])]
+  y = [int(line[1]), int(line[3])]
 
-    line_max = max(x) if max(x) > max(y) else max(y)
-    if line_max > dim_max -1:
-      dim_max = line_max +1
+  lines.append([x, y])
 
-    if (not STRAIGHLINES or x[0] == x[1] or y[0] == y[1]):
-      lines.append([x, y])
+  # find largest number, to set map dimension.
+  line_max = max(x) if max(x) > max(y) else max(y)
+  if line_max > dim_max -1:
+    dim_max = line_max +1
 
 map = np.zeros((dim_max, dim_max), dtype=np.int64)
 
+# Plot the lines on map
 for [x, y] in lines:
-  x.sort()
-  y.sort()
-
-  for i in range(x[0], x[1]+1):
-    for j in range(y[0], y[1]+1):
-      map[j][i] += 1
+  dx, dy = x[1]-x[0], y[1] - y[0]
+  dir_x = dir_y = 0
+  if dx != 0:
+    dir_x = dx / abs(dx)
+  if dy != 0:
+    dir_y = dy/ abs(dy)
+  
+  for i in range(0, max(abs(dx), abs(dy)) + 1):
+    map[y[0] + int(i*dir_y)][x[0] + int(i*dir_x)] += 1
 
 count_arr = np.count_nonzero(map >= 2)
 print(f"Part 1: Amount of crossing of at least two lines {count_arr}")
 
-
-for x1, y1 in lines:
-  if (not STRAIGHLINES or x1[0] == x1[1] or y1[0] == y1[1]):
-    plt.plot(x1,y1)
-
+plt.imshow(map >= 1)
 plt.show()
-
