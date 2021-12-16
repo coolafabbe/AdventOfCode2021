980c1d287796b28ee165dbd646ab4fd998c6762e
AHugoson <ahugoon@gmail.com>
2021-12-16 02:57:16 +0100

    Completed Day 14 https://www.youtube.com/watch?v=3GwjfUFyY6M

--- Aron/Day14/answer.py
+++ Aron/Day14/answer.py
@@ -4,14 +4,26 @@ import sys
 with open(sys.argv[1], "r") as file:
     entries = file.read().splitlines()
 
-template = list(entries[0])
-pairs = {p: i for p, i in [e.split(' -> ') for e in entries if '->' in e]}
+template = [entries[0][i]+entries[0][i+1] for i in range(len(entries[0])-1)]
+rules = {p: i for p, i in [e.split(' -> ') for e in entries if '->' in e]}
+pairs = {p:template.count(p) for p in rules}
 
-for step in range(10):
-    for i in range(1, len(template)*2-1, 2):
-        template.insert(i, pairs[''.join([template[i-1],template[i]])])
+def count_elements(pairs, rules, n):
 
-occurances = sorted([template.count(c) for c in set(template)])
+    elements = {v:entries[0].count(v) for v in rules.values()}
 
-print(f'Answer 1: {occurances[-1] - occurances[0]}')
+    for step in range(n):
+        new_pairs = {p:0 for p in rules}
+        for p, n in pairs.items():
+            new_pairs[p[0]+rules[p]] += n
+            new_pairs[rules[p]+p[1]] += n
+            elements[rules[p]] += n
+        pairs = new_pairs
+
+    return elements
+
+elements = count_elements(pairs, rules, 10)
+print(f'Answer 1 (10 steps): \033[92m{max(elements.values())-min(elements.values())}\033[0m')
+elements = count_elements(pairs, rules, 40)
+print(f'Answer 2 (40 steps): \033[92m{max(elements.values())-min(elements.values())}\033[0m')
 
